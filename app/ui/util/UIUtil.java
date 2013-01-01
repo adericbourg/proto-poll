b84f1e09688942cc1d95f378a685164d187aa81c
Alban Dericbourg <alban@dericbourg.net>
2013-01-01 18:05:02 +0100

    Added Gravatar support on user email.

--- app/ui/util/UIUtil.java
+++ app/ui/util/UIUtil.java
@@ -1,10 +1,14 @@
 package ui.util;
 
 import play.Play;
+import util.security.SessionUtil;
+
+import com.google.common.base.Strings;
 
 public final class UIUtil {
 
 	private static final String ROOT_URL = "application.base.url";
+	private static final String GRAVATAR_PICTURE_URL = "gravatar.picture.url";
 
 	private UIUtil() {
 		throw new AssertionError();
@@ -16,5 +20,19 @@ public final class UIUtil {
 			return rootUrl + relativeUrl.substring(1);
 		}
 		return rootUrl + relativeUrl;
+	}
+
+	public static boolean displayGravatar() {
+		if (!SessionUtil.isAuthenticated()) {
+			return false;
+		}
+		return !Strings.isNullOrEmpty(SessionUtil.currentUser().email);
+	}
+
+	public static String getGravatarUrl() {
+		return String.format(
+				Play.application().configuration()
+						.getString(GRAVATAR_PICTURE_URL),
+				MD5HexUtil.md5Hex(SessionUtil.currentUser().email));
 	}
 }
