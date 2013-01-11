08c18785c245c3bd18a83c24ec0c4223116c3853
Alban Dericbourg <alban@dericbourg.net>
2013-01-11 16:06:41 +0100

    Create event unit tests.

--- test/services/EventServiceTest.java
+++ test/services/EventServiceTest.java
@@ -3,9 +3,15 @@ package services;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import models.Event;
+import models.EventChoice;
 import models.User;
 
+import org.joda.time.LocalDate;
 import org.junit.Test;
 
 import util.UserTestUtil;
@@ -15,10 +21,10 @@ public class EventServiceTest extends ProtoPollTest {
 	@Test
 	public void testCreateEventAnonymousUser() {
 		// Act.
-		Event event = createEvent();
+		final Event event = createEvent();
 
 		// Assert.
-		Event loadedEvent = EventService.getEvent(event.id);
+		final Event loadedEvent = EventService.getEvent(event.id);
 		assertNotNull(loadedEvent);
 		assertNull(loadedEvent.userCreator);
 		assertEquals(0, loadedEvent.dates.size());
@@ -27,46 +33,44 @@ public class EventServiceTest extends ProtoPollTest {
 	@Test
 	public void testCreateEventRegistered() {
 		// Prepare.
-		User user = UserTestUtil.getauthenticatedUser();
+		final User user = UserTestUtil.getauthenticatedUser();
 
 		// Act.
-		Event event = createEvent();
+		final Event event = createEvent();
 
 		// Assert.
-		Event loadedEvent = EventService.getEvent(event.id);
+		final Event loadedEvent = EventService.getEvent(event.id);
 		assertNotNull(loadedEvent.id);
 		assertNotNull(loadedEvent.userCreator);
 		assertEquals(user.id, loadedEvent.userCreator.id);
 		assertEquals(0, loadedEvent.dates.size());
 	}
 
-	// FIXME org.springframework.beans.InvalidPropertyException: Invalid
-	// property 'id' of bean class [models.Event]: No property 'id' found
-	// @Test
-	// public void testSetDates() {
-	// // Prepare.
-	// Event event = createEvent();
-	//
-	// List<EventChoice> dates = new ArrayList<EventChoice>();
-	//
-	// EventChoice date1 = new EventChoice();
-	// date1.date = LocalDate.now();
-	// dates.add(date1);
-	//
-	// EventChoice date2 = new EventChoice();
-	// date2.date = LocalDate.now().plusWeeks(1);
-	// dates.add(date2);
-	//
-	// // Act.
-	// EventService.saveDates(event.id, dates);
-	//
-	// // Assert.
-	// Event loadedEvent = EventService.getEventWithChoices(event.id);
-	// assertEquals(dates.size(), loadedEvent.dates.size());
-	// }
+	@Test
+	public void testSetDates() {
+		// Prepare.
+		final Event event = createEvent();
+
+		final List<EventChoice> dates = new ArrayList<EventChoice>();
+
+		final EventChoice date1 = new EventChoice();
+		date1.date = LocalDate.now();
+		dates.add(date1);
+
+		final EventChoice date2 = new EventChoice();
+		date2.date = LocalDate.now().plusWeeks(1);
+		dates.add(date2);
+
+		// Act.
+		EventService.saveDates(event.id, dates);
+
+		// Assert.
+		final Event loadedEvent = EventService.getEventWithChoices(event.id);
+		assertEquals(dates.size(), loadedEvent.dates.size());
+	}
 
 	private Event createEvent() {
-		Event event = new Event();
+		final Event event = new Event();
 		event.title = "event title";
 		EventService.createEvent(event);
 		return event;
