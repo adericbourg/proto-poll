5cc985435c3dec85ec97c77578a34dea46db3b1a
Alban Dericbourg <alban@dericbourg.net>
2013-01-25 16:22:59 +0100

    Dates (choices) from event are populated by a FINDER.byId(). Removed unnecessary code.

--- app/services/EventService.java
+++ app/services/EventService.java
@@ -8,8 +8,6 @@ import models.EventChoice;
 import play.db.ebean.Model.Finder;
 import util.security.SessionUtil;
 
-import com.avaje.ebean.Ebean;
-
 public class EventService {
 
 	private static final Finder<Long, Event> EVENT_FINDER = new Finder<Long, Event>(
@@ -23,11 +21,6 @@ public class EventService {
 
 	public static Event getEvent(Long id) {
 		return EVENT_FINDER.byId(id);
-	}
-
-	public static Event getEventWithChoices(Long id) {
-		return Ebean.find(Event.class).fetch("dates").where().eq("id", id)
-				.findUnique();
 	}
 
 	public static void saveDates(Long eventId, Collection<EventChoice> dates) {
