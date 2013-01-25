b5cc77780b65a19388c4ee4890913a44d1362be2
Alban Dericbourg <alban@dericbourg.net>
2013-01-25 16:40:54 +0100

    Event answer wiring.

--- app/controllers/AnswerEvent.java
+++ app/controllers/AnswerEvent.java
@@ -2,11 +2,21 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import services.EventService;
+import util.security.SessionUtil;
+import views.html.eventAnswer;
 
 public class AnswerEvent extends Controller {
 
 	public static Result view(Long id) {
+		// TODO Gather results.
+		return ok(eventAnswer.render(SessionUtil.currentUser(),
+				EventService.getEvent(id)));
+	}
+
+	public static Result answer(Long id) {
 		// TODO
 		return ok();
 	}
+
 }
