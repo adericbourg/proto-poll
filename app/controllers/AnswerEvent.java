dd73e87f05539e4b017353013e4375b57d21649b
Alban Dericbourg <alban@dericbourg.net>
2013-01-25 16:58:07 +0100

    Save event answer.

--- app/controllers/AnswerEvent.java
+++ app/controllers/AnswerEvent.java
@@ -1,26 +1,65 @@
 package controllers;
 
+import static ui.tags.Messages.error;
+import static ui.tags.Messages.info;
+
+import java.util.HashSet;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import models.Event;
 import models.EventAnswer;
 import models.EventAnswerDetail;
 import models.PollResults;
+import play.api.templates.Html;
+import play.data.DynamicForm;
 import play.mvc.Controller;
 import play.mvc.Result;
 import services.EventService;
+import services.exception.AnonymousUserAlreadyAnsweredPoll;
 import util.security.SessionUtil;
 import views.html.eventAnswer;
 
+import com.google.common.base.Strings;
+
 public class AnswerEvent extends Controller {
 
+	private static final String USERNAME_KEY = "data[username]";
+
 	public static Result view(Long id) {
-		// TODO Gather results.
-		return ok(eventAnswer.render(SessionUtil.currentUser(),
-				EventService.getEvent(id), getPollResults(id)));
+		return ok(getEventViewContent(id));
+	}
+
+	private static Html getEventViewContent(Long id) {
+		return eventAnswer.render(SessionUtil.currentUser(),
+				EventService.getEvent(id), getPollResults(id));
 	}
 
 	public static Result answer(Long id) {
-		// TODO
-		return ok();
+		DynamicForm form = form().bindFromRequest();
+		Set<Long> choices = new HashSet<Long>();
+		for (Entry<String, String> entry : form.data().entrySet()) {
+			if (!isUsername(entry.getKey())) {
+				choices.add(Long.valueOf(entry.getValue()));
+			}
+		}
+		if (SessionUtil.isAuthenticated()) {
+			EventService.answerEvent(id, choices);
+		} else {
+			String username = form.data().get(USERNAME_KEY);
+			if (Strings.isNullOrEmpty(username)) {
+				error("Choose a user name.");
+				return badRequest(getEventViewContent(id));
+			}
+			try {
+				EventService.answerEvent(username, id, choices);
+			} catch (AnonymousUserAlreadyAnsweredPoll e) {
+				error("Your user name has already been used by someone else. If you want to be able to modifiy your answers, you have to be registered.");
+				return badRequest(getEventViewContent(id));
+			}
+		}
+		info("Thank you for answering!");
+		return view(id);
 	}
 
 	private static PollResults getPollResults(Long pollId) {
@@ -36,4 +75,7 @@ public class AnswerEvent extends Controller {
 		return results;
 	}
 
+	private static boolean isUsername(String key) {
+		return USERNAME_KEY.equals(key);
+	}
 }
