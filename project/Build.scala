f23525360470c86e4848ac22e81a442931cbe1c7
Alban Dericbourg <alban@dericbourg.net>
2013-02-01 10:47:08 +0100

    Clean Build.scala.

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,13 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "ProtoPoll"
-    val appVersion      = "0.1.0-SNAPSHOT"
+  val appName = "ProtoPoll"
+  val appVersion = "0.1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-      "commons-codec" % "commons-codec" % "1.7",
-      "org.mockito" % "mockito-all" % "1.9.0" % "test"
-    )
+  val appDependencies = Seq(
+    "commons-codec" % "commons-codec" % "1.7",
+    "org.mockito" % "mockito-all" % "1.9.0" % "test")
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here    
-        routesImport += "util.binders.UuidBinder._"
-    )
-
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+    routesImport += "util.binders.UuidBinder._")
 }
