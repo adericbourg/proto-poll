67251287c2cd8eeac5622a67978b6517e2993039
Alban Dericbourg <alban@dericbourg.net>
2013-03-24 19:46:07 +0100

    Release version 0.1.0.

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ import JacocoPlugin._
 object ApplicationBuild extends Build {
 
   val appName = "ProtoPoll"
-  val appVersion = "0.1.0-SNAPSHOT"
+  val appVersion = "0.1.0"
 
   val appDependencies = Seq(
     javaCore, 
