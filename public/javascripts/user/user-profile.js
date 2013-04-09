1d6f11f1cc49295301aa885cfa916c75fa4f736c
Alban Dericbourg <alban@dericbourg.net>
2013-04-09 12:04:56 +0200

    Bugfix: Javascript errors on user profile since JS refactoring.

---
+++ public/javascripts/user/user-profile.js
@@ -0,0 +1,19 @@
+$(function() {
+	$('#user-profile-tabs a').click(function (e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+	
+	// ----------------------------
+	//  User informations
+	// ----------------------------
+	
+	// Put modal info link info form.
+	$(document).ready(function() {
+		registerGravatarTipLink()
+	});
+
+	function registerGravatarTipLink() {
+		$('#avatarEmail_field .help-inline').append($('#gravatar-tip-link'))
+	}
+});
