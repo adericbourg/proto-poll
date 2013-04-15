7d84a713bdff901b3da2531af6db72505ae0665b
Alban Dericbourg <alban@dericbourg.net>
2013-04-15 18:21:05 +0200

    Question choices: up/down icons.

--- public/javascripts/question/question-add-choices.js
+++ public/javascripts/question/question-add-choices.js
@@ -1,7 +1,7 @@
 function QuestionAddChoiceCtrl($scope) {
 	$scope.choices = [];
 	
-	$scope.addChoice = function () {
+	$scope.addChoice = function() {
 		if ($scope.choiceText != null && $scope.choiceText != "") {
 			$scope.choices.push({label: $scope.choiceText, sortOrder: $scope.count()});
 			$scope.choiceText = '';
@@ -9,9 +9,36 @@ function QuestionAddChoiceCtrl($scope) {
 	};
 	
 	$scope.removeChoice = function(choice) {
-		var index = $scope.choices.indexOf(choice);
-		$scope.choices.splice(index, 1);
+		var index = indexOf(choice);
+		if (index != -1) {
+			$scope.choices.splice(index, 1);
+		}
 	};
+	
+	$scope.moveUp = function(choice) {
+		var index = indexOf(choice);
+		if (index != -1 && index > 0) {
+			$scope.removeChoice(choice);
+			$scope.choices.splice(index-1, 0, choice);
+		}
+	};
+	
+	$scope.moveDown = function(choice) {
+		var index = indexOf(choice);
+		if (index != -1 && index < $scope.choices.length-1) {
+			$scope.removeChoice(choice);
+			$scope.choices.splice(index+1, 0, choice);
+		}
+	};
+	
+	var indexOf = function(choice) {
+		for (var i = 0; i < $scope.choices.length; i++) {
+			if ($scope.choices[i].label == choice.label) {
+				return i;
+			}
+		}
+		return -1;
+	}
 	
 	$scope.count = function() {
 		var count = 0;
