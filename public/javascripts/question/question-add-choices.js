4fd0b926c7b6d8d7a26513d4608cfbc9fb724682
Alban Dericbourg <alban@dericbourg.net>
2013-04-16 14:12:06 +0200

    Perf.

--- public/javascripts/question/question-add-choices.js
+++ public/javascripts/question/question-add-choices.js
@@ -1,20 +1,20 @@
 function QuestionAddChoiceCtrl($scope) {
 	$scope.choices = [];
-	
+
 	$scope.addChoice = function() {
 		if ($scope.choiceText != null && $scope.choiceText != "") {
 			$scope.choices.push({label: $scope.choiceText, sortOrder: $scope.count()});
 			$scope.choiceText = '';
 		}
 	};
-	
+
 	$scope.removeChoice = function(choice) {
 		var index = indexOf(choice);
 		if (index != -1) {
 			$scope.choices.splice(index, 1);
 		}
 	};
-	
+
 	$scope.moveUp = function(choice) {
 		var index = indexOf(choice);
 		if (index != -1 && index > 0) {
@@ -22,7 +22,7 @@ function QuestionAddChoiceCtrl($scope) {
 			$scope.choices.splice(index-1, 0, choice);
 		}
 	};
-	
+
 	$scope.moveDown = function(choice) {
 		var index = indexOf(choice);
 		if (index != -1 && index < $scope.choices.length-1) {
@@ -30,16 +30,17 @@ function QuestionAddChoiceCtrl($scope) {
 			$scope.choices.splice(index+1, 0, choice);
 		}
 	};
-	
+
 	var indexOf = function(choice) {
-		for (var i = 0; i < $scope.choices.length; i++) {
+		var i, len;
+		for (i = 0, len = $scope.choices.length; i < len; i++) {
 			if ($scope.choices[i].label == choice.label) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	
+
 	$scope.count = function() {
 		var count = 0;
 		angular.forEach($scope.choices, function(choice) {
